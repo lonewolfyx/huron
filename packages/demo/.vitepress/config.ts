6400ba9af3a395bfa0de7d89eab575e249a228e7
lonewolfyx <877085893@qq.com>
2025-06-06 14:13:19 +0800

    fix: add annotation description

--- packages/demo/.vitepress/config.ts
+++ packages/demo/.vitepress/config.ts
@@ -5,7 +5,9 @@ import path from 'node:path'
 export default defineConfig({
     title: 'My Awesome Project',
     description: 'A VitePress Site',
+    // 定义源目录
     srcDir: path.resolve(__dirname, '../src'),
+    // 自定义目录重写
     rewrites: {
         'content/(.*)': '(.*)'
     }

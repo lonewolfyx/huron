548cd4fb161a51b0a8e5a5778ec54e59f65b522d
lonewolfyx <877085893@qq.com>
2025-06-06 17:26:42 +0800

    fix: default vitepress theme config

--- packages/demo/.vitepress/config.ts
+++ packages/demo/.vitepress/config.ts
@@ -10,5 +10,35 @@ export default defineConfig({
     // 自定义目录重写
     rewrites: {
         'content/(.*)': '(.*)'
+    },
+    // 主题配置
+    themeConfig: {
+        siteTitle: '站点标题',
+        logo: { src: '/vitepress-logo-mini.svg', width: 24, height: 24 },
+        // 社交帐户链接
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+        // 页脚
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2019-present Evan You'
+        },
+        // 编辑链接
+        editLink: {
+            pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+            text: 'Edit this page on GitHub'
+        },
+        // 允许自定义上次更新的文本和日期格式
+        lastUpdated: {
+            text: 'Updated at',
+            formatOptions: {
+                dateStyle: 'full',
+                timeStyle: 'medium'
+            }
+        },
+        // 搜索
+        search: {
+            provider: 'local'
+        }
     }
 })

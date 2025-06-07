bc4bb2a78401e8f1bf37afd99f986cbeb37a5e02
lonewolfyx <877085893@qq.com>
2025-06-07 21:32:42 +0800

    feat: add sidebar new ui

--- packages/huron/src/theme/components/HnSidebar.vue
+++ packages/huron/src/theme/components/HnSidebar.vue
@@ -7,6 +7,32 @@
                 <SidebarGroupLabel>Projects</SidebarGroupLabel>
                 <SidebarGroupContent>
                     <SidebarMenu>
+                        <SidebarMenuItem v-for="(item, index) in items" :key="index">
+                            <SidebarMenuButton as-child>
+                                <a :href="item.url">
+                                    <span>{{ item.title }}</span>
+                                </a>
+                            </SidebarMenuButton>
+
+                            <SidebarMenuSub>
+                                <SidebarMenuSubItem v-for="(subItem, subIndex) in item.items" :key="subIndex">
+                                    <SidebarMenuSubButton as-child>
+                                        <a :href="subItem.url">
+                                            <component :is="Frame" />
+                                            <span>{{ subItem.title }}</span>
+                                        </a>
+                                    </SidebarMenuSubButton>
+                                </SidebarMenuSubItem>
+                            </SidebarMenuSub>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+
+            <SidebarGroup>
+                <SidebarGroupLabel>Projects</SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
                         <SidebarMenuItem v-for="project in projects" :key="project.name">
                             <SidebarMenuButton as-child>
                                 <a :href="project.url">
@@ -31,13 +57,144 @@ import {
     SidebarGroupLabel,
     SidebarMenu,
     SidebarMenuButton,
-    SidebarMenuItem
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem
 } from './sidebar'
 import { Frame, LifeBuoy, Map, PieChart, Send } from 'lucide-vue-next'
 
 defineOptions({
     name: 'HnSidebar'
 })
+
+const items = [
+    {
+        title: 'Getting Started',
+        url: '#',
+        items: [
+            {
+                title: 'Installation',
+                url: '#'
+            },
+            {
+                title: 'Project Structure',
+                url: '#'
+            }
+        ]
+    },
+    {
+        title: 'Building Your Application',
+        url: '#',
+        items: [
+            {
+                title: 'Routing',
+                url: '#'
+            },
+            {
+                title: 'Data Fetching',
+                url: '#',
+                isActive: true
+            },
+            {
+                title: 'Rendering',
+                url: '#'
+            },
+            {
+                title: 'Caching',
+                url: '#'
+            },
+            {
+                title: 'Styling',
+                url: '#'
+            },
+            {
+                title: 'Optimizing',
+                url: '#'
+            },
+            {
+                title: 'Configuring',
+                url: '#'
+            },
+            {
+                title: 'Testing',
+                url: '#'
+            },
+            {
+                title: 'Authentication',
+                url: '#'
+            },
+            {
+                title: 'Deploying',
+                url: '#'
+            },
+            {
+                title: 'Upgrading',
+                url: '#'
+            },
+            {
+                title: 'Examples',
+                url: '#'
+            }
+        ]
+    },
+    {
+        title: 'API Reference',
+        url: '#',
+        items: [
+            {
+                title: 'Components',
+                url: '#'
+            },
+            {
+                title: 'File Conventions',
+                url: '#'
+            },
+            {
+                title: 'Functions',
+                url: '#'
+            },
+            {
+                title: 'next.config.js Options',
+                url: '#'
+            },
+            {
+                title: 'CLI',
+                url: '#'
+            },
+            {
+                title: 'Edge Runtime',
+                url: '#'
+            }
+        ]
+    },
+    {
+        title: 'Architecture',
+        url: '#',
+        items: [
+            {
+                title: 'Accessibility',
+                url: '#'
+            },
+            {
+                title: 'Fast Refresh',
+                url: '#'
+            },
+            {
+                title: 'Next.js Compiler',
+                url: '#'
+            },
+            {
+                title: 'Supported Browsers',
+                url: '#'
+            },
+            {
+                title: 'Turbopack',
+                url: '#'
+            }
+        ]
+    }
+]
 
 const projects = [
     {
